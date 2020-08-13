a6a41f25bba6bae2b1efbe153a01a44893f3ce14
vimukthiaravinda <vimukthiaravinda18@gmail.com>
2020-08-14 02:43:19 +0530

    FullVersion1

---
+++ app/src/main/java/com/sliit/channelingapp/MakePayment.java
@@ -0,0 +1,14 @@
+package com.sliit.channelingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakePayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_payment);
+    }
+}
